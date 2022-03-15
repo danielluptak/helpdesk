282a10390ae844084117cca29f977fb6dc40d71f
danielluptak <daniel.luptak2505@gmail.com>
2022-03-15 15:18:00 +0100

    unusable code, pushing for reference only

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, UpdateView
+from django.views.generic import CreateView, UpdateView, DetailView
 # from django.contrib.auth.forms import UserChangeForm
+from .models import Profile
+from django.shortcuts import get_object_or_404
+
 from .forms import CustomUserCreationForm, CustomUserITForm, EditProfileForm
 
 class SignUpView(CreateView):
@@ -29,4 +32,17 @@ class ITRegisterView(CreateView):
     def form_valid(self, form):
         form.instance.is_it = True
         return super().form_valid(form)
+
+class ShowProfilePageView(DetailView):
+    model = Profile
+    template_name = 'registration/user_profile.html'
+
+    def get_context_data(self, *args, **kwargs):
+        users = Profile.objects.all()
+        context = super(ShowProfilePageView, self).get_context_data(*args, **kwargs)
+
+        page_user = get_object_or_404(Profile, id=self.kwargs['pk'])
+
+        context["page_user"] = page_user
+        return context
     
